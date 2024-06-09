e876c4f0af8f2e9753efe4055ee788eb2ee861c6
jmreavis <131843275+jmreavis@users.noreply.github.com>
2024-06-09 16:41:20 -0700

    created scene and added move, jump, and wall collision sounds

---
+++ FinalScene/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f; // Movement speed
+    public float jumpForce = 5f; // Jump force
+    private bool isGrounded;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        OSCHandler.Instance.Init();
+        Application.runInBackground = true;
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/stop_DSP", 1);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        float moveVertical = Input.GetAxis("Vertical"); // W/S or Up/Down
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/footstep", 1);
+        }  
+        if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/footstep", 0);
+        } 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/jump", 1);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        if (collision.gameObject.CompareTag("Cube"))
+        {
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/thunder", 1);
+        }
+      
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+        if (collision.gameObject.CompareTag("Cube"))
+        {
+            OSCHandler.Instance.SendMessageToClient("pd", "/unity/thunder", 0);
+        }
+    }
+
+    [InitializeOnLoad]
+    public static class PlayModeStateListener
+    {
+        static PlayModeStateListener()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingPlayMode)
+            {
+                OSCHandler.Instance.SendMessageToClient("pd", "/unity/stop_DSP", 0);
+                Debug.Log("Exiting play mode");
+            }
+        }
+    }
+}
